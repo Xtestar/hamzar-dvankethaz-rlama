0a44ab8e098115f1ed66bf4932ebca97e4f6a6de
Hamza Rıdvan <hdargin38@gmail.com>
2023-05-08 22:29:32 +0300

    vize

---
+++ hamzaridvan/ViewModel/SonucModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace hamzaridvan.ViewModel
+{
+    public class SonucModel
+    {
+        public bool islem { get; set; }
+
+        public string mesaj { get; set; }
+    }
+}
